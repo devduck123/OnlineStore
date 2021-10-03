171a1f6ac2b26d15bc65d09a5fa982a620c92861
tommy <tommyho@cpp.edu>
2021-10-03 01:31:20 -0700

    working

--- Category.aspx.vb
+++ Category.aspx.vb
@@ -5,7 +5,7 @@
         If Request.QueryString("MainCategoryID") <> "" Then
             lblMainCategoryName.Text = Request.QueryString("MainCategoryName")
             sqlDSSubCategory.SelectCommand = "SELECT * FROM [Category] WHERE Parent = " & CInt(Request.QueryString("MainCategoryID"))
-            lblProductListType.Text = "Featured Products for Subcategory: " + Request.QueryString("MainCategoryName")
+            lblProductListType.Text = "Featured Products for " + Request.QueryString("MainCategoryName")
             sqlDSProductList.SelectCommand = "SELECT * FROM [Product] WHERE MainCategoryID = " & CInt(Request.QueryString("MainCategoryID")) & " AND ProductFeatured = 'Y'"
             'DEBUGGING? Response.Write(sqlDSProductList.SelectCommand)
         End If
